00593e2bf8fda8e9a99ee5ac073f490db1470a64
Colin Seymour <colin@symr.io>
2022-05-10 11:01:05 +0100

    Add more stuffs

---
+++ foo/G.js
@@ -0,0 +1,6 @@
+ 	user_pref("devtools.enabled", true);
+	user_pref("devtools.chrome.enabled", true);
+	user_pref("devtools.debugger.prompt-connection", false);
+	user_pref("devtools.debugger.remote-enabled", true);
+	user_pref("devtools.experiment.f12.shortcut_disabled", true);
+	user_pref("devtools.policy.disabled", true);
